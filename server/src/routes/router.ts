a2651dcfa5052068fd1efb7310b1eac30054001c
João Victor Batista <jvtexbat@hotmail.com>
2021-07-16 12:01:39 -0300

    #7 Create post logic
    Co-authored-by: João Gabriel <joaogabriel.c.matos@gmail.com>
    Co-authored-by: Pedro Miguel <pedrommartins.2014@gmail.com>

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
 // importa as rotas
-import userRoutes from './userRoutes'
+import userRoutes from './userRoutes';
+import postRoutes from './postRoutes';
 
 const router = Router();
 
 router.use('/user', userRoutes);
 
+router.use('/post', postRoutes);
 
 router.get('/register', function(req, res) {
     res.send('Cadastrar');
