9c57e90b3d2aec0cc78cf6269e9684060d7188a7
Emily Sousa <diassousa@yahoo.com.br>
2021-07-18 16:48:43 -0300

    add spaces

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,58 +1,49 @@
 import { Request, Response } from "express";
-import * as jwt from 'jsonwebtoken';
-import * as authConfig from '../config/authConfig.json';
+import * as jwt from "jsonwebtoken";
+import * as authConfig from "../config/authConfig.json";
 
 interface Idata {
-    id: string;
-    iat: number,
-    exp: number
+  id: string;
+  iat: number;
+  exp: number;
 }
 export default class Auth {
+  tokenGenerator = async (_id: string) => {
+    const token = jwt.sign({ id: _id }, authConfig.secret, {
+      expiresIn: 86400,
+    });
 
-    tokenGenerator = async (_id: string) => {
+    return token;
+  };
 
-        const token = jwt.sign({ id: _id }, authConfig.secret, { expiresIn: 86400 });
-        // const token = jwt.sign({ id: _id }, authConfig.secret, { expiresIn: 1 });
-
-        return token;
+  tokenDecoder = async (token: string) => {
+    try {
+      const data = await jwt.verify(token, authConfig.secret);
+      return data;
+    } catch (error) {
+      return undefined;
     }
+  };
 
-    tokenDecoder = async (token: string) => {
-        try {
-            const data = await jwt.verify(token, authConfig.secret);
-            return data;
-        } catch (error) {
-            return undefined;
-        }
+  autenticate = async (req: Request, res: Response, next: () => void) => {
+    try {
+      var token = req.body.token;
+      if (!token) token = req.params.token;
+      if (!token) token = req.query.token;
+
+      if (!token) res.status(401).send({ message: "Token não informado" });
+
+      const data = await jwt.verify(token, authConfig.secret);
+
+      req.userId = (<Idata>data).id;
+
+      next();
+    } catch (error) {
+      if (error.message === "invalid signature")
+        res.status(401).send({ message: "Token invalido" });
+      else {
+        res.status(401).send({ error: error.message });
+      }
     }
-
-    autenticate = async (req: Request, res: Response, next: () => void) => {
-        try {
-
-            var token = req.body.token;
-            if (!token)
-                token = req.params.token;
-            if (!token)
-                token = req.query.token;
-
-
-            if (!token)
-                res.status(401).send({ message: "Token não informado" });
-
-            const data = await jwt.verify(token, authConfig.secret);
-
-            req.userId = (<Idata>data).id;
-
-            next();
-
-        } catch (error) {
-            if (error.message === 'invalid signature')
-                res.status(401).send({ message: "Token invalido" });
-            else {
-                res.status(401).send({ error: error.message });
-            }
-        }
-
-    }
-
+  };
 }
--- server/src/resources/mail/templates.ts
+++ server/src/resources/mail/templates.ts
@@ -1,16 +1,13 @@
 export const forgotPasswordEmail = (token: string, userName: string) => {
+  const url = "http://localhost:3000/recover";
 
-    const url = "http://localhost:3000/recover";
-
-    return (`<p>Para recuperar sua senha,clique neste <a href='${url}/${userName}/${token}' > link</a> </p>`)
-}
-
-
+  return `<p>Para recuperar sua senha, clique neste <a href='${url}/${userName}/${token}' > link</a> </p>`;
+};
 
 export const validateEmail = (token: string, userName: string) => {
-    const url = "http://localhost:3000/validate";
+  const url = "http://localhost:3000/validate";
 
-    return (`<p>
+  return `<p>
     Para Validar sua conta, clique neste <a href="${url}/${userName}/${token}">link</a> .
-            </p>`)
-}
+            </p>`;
+};
